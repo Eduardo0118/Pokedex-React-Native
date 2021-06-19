4e78bcff32026ca1e7a12ccb30bd7aaddea8007f
Eduardo Jose <eduardojose@avamobile.com.br>
2021-06-19 13:25:23 -0300

    Initial commit

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Home = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>Initial commit</S.Title>
+    </S.Wrapper>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+import { View, Text } from 'react-native';
+
+export const Wrapper = styled(View)`
+  flex: 1;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled(Text)`
+  font-size: 20px;
+  color: #2b2b2b;
+`;
